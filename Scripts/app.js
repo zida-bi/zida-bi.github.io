628f2874fe68dd32526d711fdde7e7f68a286aa1
zida-bi <zida_bi@hotmail.com>
2015-01-28 11:41:09 -0500

    Copy Project

---
+++ Scripts/app.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="C:\Users\zida\Desktop\Assignment01\Assignment01\assets/sliderengine/jquery.js" />
+/// <reference path="Libs/foundation.min.js" />
+/// <reference path="jquery-2.1.3.min.js" />
+function init() {
+
+    $(document).foundation();
+
+}
+

eff178897a621535a53aab79dc0001963cf295bc
zida-bi <zida_bi@hotmail.com>
2015-02-27 21:42:12 -0500

    MobileSite-Add

---
+++ mobile/Scripts/_references.js
@@ -0,0 +1,7 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="app.js" />
+/// <reference path="jquery-1.8.0.js" />
+/// <reference path="jquery.mobile-1.4.5.js" />
+/// <reference path="../sliderengine/jquery.js" />
+/// <reference path="../sliderengine/initslider-1.js" />
+/// <reference path="../sliderengine/amazingslider.js" />
---
+++ mobile/Scripts/app.js
@@ -0,0 +1,7 @@
+﻿///File name: app.js
+///Author: Zida Bi
+///Website name: Zida's Home
+///Description: javascript file that enables webpages functionallity
+
+/// <reference path="jquery-1.8.0.min.js" />
+/// <reference path="jquery.mobile-1.4.5.min.js" />
